9364c50df938f7c7ac9aea240e529682ba735de8
GigaJuan13 <eric.jonsson@elev.ga.ntig.se>
2023-10-05 11:29:06 +0200

    Jobbat med en övning

---
+++ src/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+    // Attributes
+    private String name;
+    private String adress;
+    private int numCourses = 0;
+    private String[] courses = new String[30];
+    private int[] grades = new int[30];
+
+    // Constructor
+
+
+    // Methods
+
+
+    // Getters and setters
+
+
+}
